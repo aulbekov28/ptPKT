65a3c275515c09810d2de665e545dfe19e1c9ee2
aulbekov <aulbekov28@gmail.com>
2020-03-03 15:37:26 +0300

    moving Accounts logic from controller to service

--- src/ptPKT.Core/Interfaces/Account/IAccountService.cs
+++ src/ptPKT.Core/Interfaces/Account/IAccountService.cs
@@ -1,7 +1,18 @@
-﻿namespace ptPKT.Core.Interfaces.Account
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ptPKT.Core.Identity;
+using ptPKT.Core.Services.Account;
+
+namespace ptPKT.Core.Interfaces.Account
 {
     public interface IAccountService
     {
-        
+        Task ConfirmEmailAsync(string confirmationToken);
+        Task ForgotPasswordAsync(UserLoginModel user);
+        IEnumerable<AppUser> GetUsers();
+        Task<LoginResult> ResetPasswordAsync(UserLoginModel user);
+        Task<LoginResult> SignIn(UserLoginModel model);
+        Task SignOut();
+        Task<LoginResult> SignUp(UserRegistedModel model);
     }
 }
--- src/ptPKT.Core/Services/Account/AccountService.cs
+++ src/ptPKT.Core/Services/Account/AccountService.cs
@@ -1,10 +1,141 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using ptPKT.Core.Identity;
+using ptPKT.Core.Interfaces.Account;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Authentication;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ptPKT.Core.Services.Account
 {
-    class AccountService
+    public class AccountService : IAccountService
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountService(UserManager<AppUser> _userManager)
+        {
+            this._userManager = _userManager;
+        }
+
+        public async Task<LoginResult> SignIn(UserLoginModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+                throw new AuthenticationException();
+
+            var result = await _userManager.CheckPasswordAsync(user, model.Password);
+            if (!result)
+                throw new AuthenticationException();
+
+            var response = new LoginResult()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                SecondName = user.SecondName,
+                Token = GenerateToken(user)
+            };
+
+            return response;
+        }
+
+        public async Task<LoginResult> SignUp(UserRegistedModel model)
+        {
+            var newUser = new AppUser
+            {
+                UserName = model.UserName,
+                FirstName = model.FirstName,
+                SecondName = model.SecondName,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+
+            if (!result.Succeeded)
+                throw new OperationCanceledException();
+
+            var response = new LoginResult()
+            {
+                Id = newUser.Id,
+                FirstName = newUser.FirstName,
+                SecondName = newUser.SecondName,
+                Token = GenerateToken(newUser)
+            };
+
+            return response;
+        }
+
+        public async Task SignOut()
+        {
+            // return Success Result with fake Token ???
+        }
+
+        public IEnumerable<AppUser> GetUsers()
+        {
+            return _userManager.Users;
+        }
+
+        public async Task ConfirmEmailAsync(string confirmationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ForgotPasswordAsync(UserLoginModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LoginResult> ResetPasswordAsync(UserLoginModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string GenerateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("name", user.UserName),
+                new Claim("id", user.Id.ToString())
+            };
+
+            var signingKey =
+                new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET")));
+            var creds = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(60);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+    public class LoginResult
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string UserName { get; set; }
+        public string Token { get; set; }
+    }
+
+    public class UserRegistedModel
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string UserName { get; set; } 
+        public string Email { get; set; } 
+        public string Password { get; set; } 
+    }
+
+    public class UserLoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
